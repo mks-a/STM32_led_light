8499c36d09bc8dceefe91dcb496ae34f639aba1b
mks-a <8500434+mks-a@users.noreply.github.com>
2017-08-02 18:24:37 +0300

    Changes 170802
    Trying to set RCC registers

--- code/main.s
+++ code/main.s
@@ -1,21 +1,31 @@
+.thumb
 @This turns on Unified Assembly Language which is required to get all the features of Thumb-2.
 .syntax unified					@ without this line "orr r0, r0, #0x200000" will give an error "unshifted register required" 
 
-.equ STACKINIT, 0x00028000
+.equ STACKINIT, 0x20005000		@ end of SRAM 20kb
 
 .text
 .org 0x00000000					@ this not work!!!
 SP: .word STACKINIT				@ 20kb of RAM, so I will put stack pointer to the end of it
-RESET: .word main @ + 1
+RESET: .word main + 1
+NMI_HANDLER: .word nmi_fault + 1
+HARD_FAULT: .word hard_fault + 1
+MEMORY_FAULT: .word memory_fault + 1
+BUS_FAULT: .word bus_fault + 1
+USAGE_FAULT: .word usage_fault + 1
 
 .ifndef LED_DEF
 .include "stm32f103c8t6_core/led.inc"
 .endif
 
 main:
-	@bl led_init					@ call led init function
+	push {lr}
+	bl led_init					@ call led init function
+	pop {lr}
 _main_loop:
-	@bl led_light_on				@ branch to led_light_on and store next address that come after branch to link register (LR) to return (it used as call in x86 or AVR assembler)
+	push {lr}
+	bl led_light_on				@ branch to led_light_on and store next address that come after branch to link register (LR) to return (it used as call in x86 or AVR assembler)
+	pop {lr}
 	b _main_loop				@ branch to _main_loop and not load return address to link register (LR)
 	@ return from function
 	bx lr						@ indirect branch to link register address
@@ -23,6 +33,25 @@ _main_loop:
 @delay:
 	@ return from function
 @	bx lr
+
+nmi_fault:
+	bx lr
+	
+hard_fault:
+	bx lr
+
+memory_fault:
+	bx lr
+
+bus_fault:
+	bx lr
+
+usage_fault:
+	bx lr	
+
+@ this is dummy function that just return from interrupt
+returtn_form_interrupt:
+	bx lr
 	
 .end
 	
